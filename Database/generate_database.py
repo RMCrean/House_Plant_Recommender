0daac762ec9ae2a66b76f70f3922e5bba0a8fb55
Rory Crean <rory.m.crean@gmail.com>
2022-09-16 15:14:01 +0200

    Update generate_database.py
    Final version used successfully to get all plant names and first block of 95 results with google's search API.

--- Database/generate_database.py
+++ Database/generate_database.py
@@ -12,13 +12,8 @@ And store this in a database (both those that "worked" and those that did not).
 3.
 """
 import configparser
-import re
 import requests
-from typing import Tuple
-from time import sleep
 from bs4 import BeautifulSoup
-from googleapiclient.discovery import build
-import pandas as pd
 import sqlite3
 
 import helper_functions
@@ -28,11 +23,21 @@ config = configparser.ConfigParser()
 config.read("Database/config.ini")
 SEARCH_ENGINE_ID = config["Google Params"]["SEARCH_ENGINE_ID"]
 API_KEY = config["Google Params"]["API_KEY"]
+DATABASE_LOC = r"C:\Users\Rory Crean\Dropbox (lkgroup)\Backup_HardDrive\Postdoc\PyForFun\House_Plant_Recommender\Database\house_plants.db"
+
+green_url = r"https://www.blomsterlandet.se/produkter/vaxter/inomhus/grona-vaxter/?page=50&sorting=Name&filterDefaults=false"
+flowering_url = r"https://www.blomsterlandet.se/produkter/vaxter/inomhus/blommande-vaxter/?page=50&sorting=Name&filterDefaults=false"
+
+
+# Which range of list items to run through the google search API.
+SUBSET_START = 0
+SUBSET_END = 95
 
 
 def get_blomsterlandet_plants(url: str) -> set:
     """
-    Using the website: https://www.blomsterlandet.se to identify all the houseplants they have available.
+    Using the website: https://www.blomsterlandet.se to identify all the houseplants they have available
+    on a given product page. Specifically extracts the latin name assinged to each product.
     Helper function for step 1.
 
     Parameters
@@ -51,21 +56,21 @@ def get_blomsterlandet_plants(url: str) -> set:
     science_section = soup.find_all(
         'p', class_="ProductCardBodystyled__ScientificName-lrzi29-3 flSCLr")
 
-    return {science_name.get_text() for science_name in science_section}
+    # (first make set to remove duplicates)
+    return list({science_name.get_text() for science_name in science_section})
 
 
 if __name__ == '__main__':
 
-    blomsterlandet_url = "https://www.blomsterlandet.se/produkter/vaxter/inomhus/grona-vaxter/?page=50&sorting=Name&filterDefaults=false"
-    database_loc = r"C:\Users\Rory Crean\Dropbox (lkgroup)\Backup_HardDrive\Postdoc\PyForFun\House_Plant_Recommender\Database\house_plants.db"
-
     # 1. Generate a set of all Latin_names available to purchase from blomsterlandet.se
-    plant_names = get_blomsterlandet_plants(url=blomsterlandet_url)
+    green_plants = get_blomsterlandet_plants(url=green_url)
+    flowering_plants = get_blomsterlandet_plants(url=flowering_url)
+    plant_names = list(set(green_plants + flowering_plants))
 
     # reformat to that desired by the database
     plant_names_db = [(name, ) for name in plant_names]
 
-    conn = sqlite3.connect(database_loc)
+    conn = sqlite3.connect(DATABASE_LOC)
     c = conn.cursor()
 
     c.execute("""DROP TABLE IF EXISTS latin_names""")
@@ -76,11 +81,10 @@ if __name__ == '__main__':
         )
     """)
 
-    c.executemany("""
-    INSERT INTO latin_names VALUES (?)""", plant_names_db)
+    c.executemany("""INSERT INTO latin_names VALUES (?)""", plant_names_db)
     conn.commit()
 
-    # read back out of database to ensure use same order for each run.
+    # read back out of database to ensure use same order for each follow up run.
     database_names = []
     c.execute("""SELECT * FROM 'latin_names'""")
     for row in c.fetchall():
@@ -91,14 +95,14 @@ if __name__ == '__main__':
     # reformat for easy operations.
     latin_names = [name[0] for name in database_names]
 
-    # 2. Run google search on the first X set of plants
-    latin_names_subset = list(latin_names)[0:5]
+    # 2. Run google search on the first 95 plants
+    latin_names_subset = list(latin_names)[SUBSET_START:SUBSET_END]
 
     plant_with_link, plant_no_link = helper_functions.search_for_plant(
         latin_names=latin_names_subset, api_key=API_KEY, search_engine_id=SEARCH_ENGINE_ID)
 
     # 2.5 Save these to the database
-    conn = sqlite3.connect(database_loc)
+    conn = sqlite3.connect(DATABASE_LOC)
     c = conn.cursor()
 
     c.execute("""DROP TABLE IF EXISTS hyperlinks""")
@@ -115,7 +119,20 @@ if __name__ == '__main__':
     conn.commit()
 
     c.execute("""SELECT * FROM 'hyperlinks'""")
+
+    output = []
+    found, not_found = 0, 0
+
     for row in c.fetchall():
-        print(row)
+        output.append(row)
+
+        if row[1] == "no link found":
+            not_found += 1
+        else:
+            found += 1
+
+    print(f"Total number of links now searched: {len(output)}")
+    print(f"Number of links found: {found}")
+    print(f"Number of links not found: {not_found}")
 
     c.close()
